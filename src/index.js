9c0ca0c0049a51712dd2e16e0cc409dd087142ad
Liseth-Poma <84851110+Liseth-Poma@users.noreply.github.com>
2025-05-19 09:00:39 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-const { count } = require('console');
-const express = require('express'); //express framework
-const path = require('path');
-const { createServer } = require('http'); //HTTp Server
-const { Server }= require('socket.io');//socket.io
+const express = require("express"); // Express framework
+const path = require("path"); // Path module
+const { createServer } = require("http"); // HTTP server
+const { Server } = require("socket.io"); // Socket.IO
 
-const app = express();//crear una app con express
-const httpServer = createServer(app); //crear un http server
-const io= new Server(httpServer); //creaer un servidor soket.io
+const app = express(); // Create an Express application
+const httpServer = createServer(app); // Create an HTTP server
+const io = new Server(httpServer); // Create a Socket.IO server
 
 app.use(express.static(path.join(__dirname, "views")));
 
+const socketOnline = []; // Array to store online sockets
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html"); // Serve the index.html file
+}); // Define a route for the root URL
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+io.on("connection", (socket) => {
+  socketOnline.push(socket.id); // Add the socket ID to the array
+  // Emisión básica
+  socket.emit("Bienvenido", "Ahora estás conectado 🥸"); // Send a message to the client
+  socket.on("server", (data) => {
+    console.log(data); // Log the message received from the client
+  }); // Listen for messages from the client
+
+  // Emisión a todos los clientes
+  io.emit("everyone", socket.id + " se ha conectado!!"); // Send a message to all connected clients
+
+  // Emisión a un cliente específico
+  socket.on("last", (massage) => {
+    const lastSocket = socketOnline[socketOnline.length - 1]; // Get the last socket ID
+    io.to(lastSocket).emit("saludoUnico", massage); // Send a message to the last socket
+  }); // Listen for messages from the client
+
+  socket.emit("on", "Hola AD");
+  socket.emit("on", "Hola AD");
 });
-
-io.on('connection', (socket) =>{
-    console.log("users conectados verificado desde el cliente: ", io.engine.clientsCount);
-    console.log("Id del socket conectado:", socket.id);
-
-    socket.on("disconnect", ()=>{
-        console.log("El socket " + socket.id + " se ha desconectado desde el cliente")
-    });
-});
-
-httpServer.listen(3000);
+httpServer.listen(3000); // Start the HTTP server on port 3000
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,4 +1,44 @@
 const socket = io();
-socket.on("connect", ()=>{
-    console.log("Usuarios conectado verificado desde el cliente");
+socket.on("Bienvenido", (data) => {
+    /* console.log(data); */
+    const text = document.querySelector("#text");
+    text.textContent = data;
+
+    const emitToServer = document.querySelector("#emit-to-server");
+
+    emitToServer.addEventListener("click", () => {
+        socket.emit("server", "Hola desde el servider");
+    });
+    socket.on("everyone", (massage) => {
+        console.log(massage);
+    });
+
+    const emitToLast = document.querySelector("#emit-to-last");
+    emitToLast.addEventListener("click", () => {
+        socket.emit("last", "Hola desde el servidor");
+    });
+
+    socket.on("saludoUnico", (massage) => {
+        console.log(massage);
+    });
+
+
+    // Diferencia entre on, once y off
+    socket.on("on", () => {
+        console.log("Se emite varias veces");
+    });
+    socket.once("once", () => {
+        console.log("Se emite una sola vez");
+    });
+
+    const listener = () => {
+        console.log("Se apago el evento");
+    };
+
+    socket.on("off", listener);
+
+    setTimeout(() => {
+        socket.off("off", listener);
+    }, 2000);
+
 });
